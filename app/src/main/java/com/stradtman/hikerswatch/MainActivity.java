b2d86feb920e941260f5397d92eeb4d29c1bf9d8
Kevin <stradtkt22@gmail.com>
2019-08-31 00:42:09 -0400

    Fin

--- app/src/main/java/com/stradtman/hikerswatch/MainActivity.java
+++ app/src/main/java/com/stradtman/hikerswatch/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,6 +17,10 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -35,11 +41,39 @@ public class MainActivity extends AppCompatActivity {
         TextView lngTextView = (TextView) findViewById(R.id.lngTextView);
         TextView accTextView = (TextView) findViewById(R.id.accTextView);
         TextView altTextView = (TextView) findViewById(R.id.altTextView);
-        TextView addTextView = (TextView) findViewById(R.id.addTextView);
         latTextView.setText("Latitude: " + location.getLatitude());
         lngTextView.setText("Longitude: " + location.getLongitude());
         altTextView.setText("Altitude: " + location.getAltitude());
         accTextView.setText("Accuracy: " + location.getAccuracy());
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try {
+            String address = "Could not find an address";
+            List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if(addressList != null && addressList.size() > 0) {
+                Log.i(TAG, "updateLocationInfo: address: " + addressList.get(0).toString());
+                address = "";
+                if(addressList.get(0).getSubThoroughfare() != null) {
+                    address += addressList.get(0).getSubThoroughfare() + " ";
+                }
+                if(addressList.get(0).getThoroughfare() != null) {
+                    address += addressList.get(0).getThoroughfare() + "\n";
+                }
+                if(addressList.get(0).getLocality() != null) {
+                    address += addressList.get(0).getLocality() + "\n";
+                }
+                if(addressList.get(0).getPostalCode() != null) {
+                    address += addressList.get(0).getPostalCode() + "\n";
+                }
+                if(addressList.get(0).getCountryName() != null) {
+                    address += addressList.get(0).getCountryName() + "\n";
+                }
+            }
+            TextView addressTextView = (TextView) findViewById(R.id.addTextView);
+            addressTextView.setText(address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void startListening() {
